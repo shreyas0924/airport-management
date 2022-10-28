18239c0b7b078253127ceb23a8524b393effc065
shreyas0924 <shreyasp0924@gmail.com>
2022-10-28 23:28:07 +0530

    bg-black

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 
 function App() {
   return(
-    <div className='text-2xl text-center mt-5 flex-center'>Airport mangament system</div>
+    <div className='text-2xl text-center mt-5 flex-center bg-black text-white'>Airport mangament system</div>
   )
 }
 
