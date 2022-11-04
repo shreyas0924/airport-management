eee8bc40a7aa9787317e2b485ea7546737463da1
shreyas0924 <shreyasp0924@gmail.com>
2022-11-04 22:11:06 +0530

    updated layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@
 function App() {
   return (
     <>
-      <nav  className='text-2xl text-center mt-5 flex-center text-[#F9F5EB]' >
+      <nav  className='text-[30px] text-center mt-5 flex-center text-white' >
         Airport Mangement System
       </nav>
 
-      <div className="">
-        <input></input>
+      <div className="m-6 p-6 flex justify-center gap-3">
+        
+        <input className="rounded-xl w-[500px] hover:border-transparent " placeholder='Eneter text'></input>
+        <button className=" text-2xl text-white border-2 border-white rounded-xl p-1 cursor-pointer   ">Search </button>
       </div>
 
     </>
-    
-
     
   );
 }
