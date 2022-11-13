1b2d9a74018f5b004656ee0906dc8f988cdcb364
shreyas0924 <shreyasp0924@gmail.com>
2022-11-13 18:41:12 +0530

    updated styles

--- components/Search.jsx
+++ components/Search.jsx
@@ -5,11 +5,11 @@ export default function Search() {
     <div>
       <div className="m-6 p-6 flex justify-center gap-3">
         <input
-          className="rounded-xl w-[500px] hover:border-transparent "
-          placeholder="Eneter text"
+          className="rounded-xl w-[500px] hover:border-transparent  "
+          placeholder="Enter text"
         ></input>
         <button className=" text-2xl text-white border-2 border-white rounded-xl p-1 cursor-pointer   ">
-          Search{" "}
+          Search
         </button>
       </div>
     </div>
