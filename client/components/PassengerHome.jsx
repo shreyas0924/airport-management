785040a6c3841662f0b8fdc56093edbb9353e251
shreyas0924 <shreyasp0924@gmail.com>
2023-01-08 14:30:35 +0530

    passenger navbar done

--- client/components/PassengerHome.jsx
+++ client/components/PassengerHome.jsx
@@ -1,5 +1,30 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import '../src/navbar.css'
 
 export default function PassengerHome() {
-  return <div>This is passenger</div>
+  const navigate = useNavigate()
+  function logout() {
+    navigate('/')
+  }
+  return (
+    <>
+      <div className='navbar'>
+        <div className='navBarContainer'>
+          <div className='navBarLogo'>
+            <button>
+              <h3 className='text-2xl'>Airport Management System</h3>
+            </button>
+          </div>
+
+          <div className='navLinks'>
+            {/* <button className='link-btn'>Home</button>
+            <button className='link-btn'>Business</button>
+            <button className='link-btn'>Add Business</button> */}
+            <button className='link-btn border-2 rounded-xl border-white border-spacing-10' onClick={logout}>Log Out</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
