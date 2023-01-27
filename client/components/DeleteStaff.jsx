102a11db747730a00f05d3b1b59e26d6371cd322
shreyas0924 <shreyasp0924@gmail.com>
2023-01-27 22:38:35 +0530

    minor

--- client/components/DeleteStaff.jsx
+++ client/components/DeleteStaff.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Form } from "react-router-dom";
-import axios from "axios";
+import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Form } from 'react-router-dom'
+import axios from 'axios'
 
 const DeleteStaff = () => {
-  const navigate = useNavigate();
-  const [empid, setEmpid] = useState("");
+  const navigate = useNavigate()
+  const [empid, setEmpid] = useState('')
 
-  const [deleteStaff, setDeleteStaff] = useState([]);
+  const [deleteStaff, setDeleteStaff] = useState([])
   function delStaff(e) {
-    e.preventDefault();
+    e.preventDefault()
 
-    const url = "http://localhost:3001/api/deletestaff";
+    const url = 'http://localhost:3001/api/deletestaff'
     try {
       axios
         .post(url, {
@@ -20,45 +20,43 @@ const DeleteStaff = () => {
         })
         .then(() =>
           setDeleteStaff([
-            // arr[] = {} ...arr => {}  [..arr, 1] => {1} = arr    [..arr, [2,3,5]}] => 1,2,3,5
             ...deleteStaff, //spread operator
             {
-              // apend below
               empid: empid,
             },
           ])
-        );
+        )
     } catch (err) {
-      console.error(err);
+      console.error(err)
     }
 
-    navigate(-1);
+    navigate(-1)
   }
   function goback() {
     navigate('/staffprofile')
   }
   return (
-    <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
-      <Form className="border-black border-2" method="post" onSubmit={delStaff}>
-        <h1 className="text-center text-2xl mt-5 mb-5">Delete Staff</h1>
+    <div className='flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5'>
+      <Form className='border-black border-2' method='post' onSubmit={delStaff}>
+        <h1 className='text-center text-2xl mt-5 mb-5'>Delete Staff</h1>
 
-        <div className="flex m-5">
+        <div className='flex m-5'>
           <input
-            className="form-control  block w-50 px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-            type="text"
-            name="name"
-            placeholder="Employee Id"
+            className='form-control  block w-50 px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
+            type='text'
+            name='name'
+            placeholder='Employee Id'
             value={empid}
             onChange={(e) => setEmpid(e.target.value)}
           />
         </div>
-        <div className="text-center lg:text-left">
+        <div className='text-center lg:text-left'>
           <button
-            type="submit"
-            className="inline-block ml-5 mb-3 px-4 py-3 bg-red-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            type='submit'
+            className='inline-block ml-5 mb-3 px-4 py-3 bg-red-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'
             onClick={delStaff}
           >
-            Delete 
+            Delete
           </button>
           <button
             className='inline-block ml-5 mb-4 px-5 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'
@@ -66,11 +64,10 @@ const DeleteStaff = () => {
           >
             Go Back
           </button>
-          
         </div>
       </Form>
     </div>
-  );
-};
+  )
+}
 
-export default DeleteStaff;
+export default DeleteStaff
